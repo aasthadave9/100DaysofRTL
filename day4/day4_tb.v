2f57db8f9392de4cd53834b770255bdea09fe46c
Aastha Dave <86701156+aasthadave9@users.noreply.github.com>
2022-12-11 13:17:28 +0100

    Create day4_tb.v

---
+++ day4/day4_tb.v
@@ -0,0 +1,24 @@
+module day4_tb();
+  
+  parameter width = 4;  //change parameter if you wish
+  
+  reg [width-1:0] bin; 
+  wire [width-1:0] gray;
+  
+  day4 #(width) DAY4 (.bin(bin), .gray(gray));
+  
+  initial begin
+    for(int i=0; i< 2**width; i=i+1)
+      begin
+        bin = i;
+        #2;
+      end
+    $finish(); 
+  end
+  
+  initial begin
+    $dumpfile("day4.vcd");
+    $dumpvars(2, day4_tb);
+  end
+  
+endmodule
