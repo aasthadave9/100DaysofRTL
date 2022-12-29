9ed06f161841213f076c2a6dbe35e84c744e9036
Aastha Dave <86701156+aasthadave9@users.noreply.github.com>
2022-12-29 12:01:09 +0100

    Update day6.v

--- day6/day6.v
+++ day6/day6.v
@@ -1,6 +1,6 @@
 module day6 (
   input clk, rst,
-  output [3:0] count 
+  output reg [3:0] count 
 );
   
   always @(posedge clk)
@@ -9,15 +9,15 @@ module day6 (
       if(!rst)
         begin
           
-          if(count >= 4'b0000 && count < 4'b1010)
+          if(count >= 4'd0 && count < 4'd10)
             begin
               count = count+1;
             end else begin
-              count <= 4'b0000;
+              count <= 4'd0;
             end
           
         end else begin
-          count <= 4'b0000;
+          count <= 4'd0;
         end
       
     end
