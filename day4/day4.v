4abfc795cc3518117e750ffe92f58abc6232c2af
Aastha Dave <86701156+aasthadave9@users.noreply.github.com>
2022-12-29 09:58:39 +0100

    Update day4.v

--- day4/day4.v
+++ day4/day4.v
@@ -1,12 +1,19 @@
-module day2(
-  input [7:0] a,
-  input [7:0] b,
-  input cin,
-  output [7:0] sum,
-  output cout
+module day4 #(
+  parameter width = 4
+  
+)(
+  input [width-1:0] bin,
+  output reg [width-1:0] gray
 );
   
-  assign sum = (a ^ b ^ cin);
-  assign cout = (a & b) | (b & cin) | (cin & a);
- 
+  assign gray[width-1] = bin[width-1];
+
+  genvar i;
+  generate
+    for(i=width-2; i>=0; i=i-1)
+      begin
+       assign gray[i] = bin[i+1] ^ bin[i]; 
+      end
+  endgenerate
+
 endmodule
