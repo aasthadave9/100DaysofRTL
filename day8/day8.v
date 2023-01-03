9d091bdab0afad3df9030681e81b444edd2819eb
Aastha Dave <86701156+aasthadave9@users.noreply.github.com>
2023-01-03 15:29:25 +0100

    Update day8.v

--- day8/day8.v
+++ day8/day8.v
@@ -1,1 +1,24 @@
+module day8 #(
+  parameter n = 8
+)(
+  input s_in, clk, rst,
+  output reg s_out
+);
+  
+  reg [n-1:0] r;
+  wire [n-1:0] r_next;
+  
+  always @(posedge clk) 
+    begin
+      if(rst) begin
+        r <= 0;
+      end else begin
+        r <= r_next;
+      end
+    end
+  
+  assign r_next = {s_in, r[n-1:1]};
+  assign s_out = r[0];
+  
+endmodule
 
